e9ae001de72da7976735f4086ee29c27ec349fa1
thomasShelby <101035052+Maxim-Zauysev@users.noreply.github.com>
2023-11-03 18:14:57 +0700

    syntax highlighting

--- src/main/java/com/zaytsev/app/fxapplication/Controllers/MainFormController.java
+++ src/main/java/com/zaytsev/app/fxapplication/Controllers/MainFormController.java
@@ -13,21 +13,22 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.model.StyleSpans;
+import org.fxmisc.richtext.model.StyleSpansBuilder;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
-import java.util.ResourceBundle;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainFormController implements Initializable {
 
     @FXML
-    private TextArea codeWindow;
+    private CodeArea codeWindow;
     @FXML
     private ComboBox<String> comboBox;
     @FXML
@@ -35,7 +36,7 @@ public class MainFormController implements Initializable {
     @FXML
     private Button startButton;
     @FXML
-    private TextArea userWindow;
+    private CodeArea userWindow;
     private final DatabaseManager databaseManager = new DatabaseManager();
 
     @FXML
@@ -44,11 +45,19 @@ public class MainFormController implements Initializable {
     private Duration duration;
     private boolean codeGenerated = false;
     private Integer userId;
+
+
+
     private void initData() {
         System.out.println(userId);
     }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        codeWindow.getStylesheets().add(getClass().getResource("/main.css").toExternalForm());
+        userWindow.getStylesheets().add(getClass().getResource("/main.css").toExternalForm());
+//        codeWindow.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+//        userWindow.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
 
         // Загрузка языков из базы данных и добавление их в ComboBox
         List<String> languages = databaseManager.getLanguages();
@@ -71,7 +80,7 @@ public class MainFormController implements Initializable {
 
         generateButton.setOnAction(event ->{
             if (comboBox.getValue() != null) {
-                userWindow.setText("");
+                userWindow.clear();
                 generateCode();
                 codeGenerated = true;
             }
@@ -126,7 +135,12 @@ public class MainFormController implements Initializable {
                 }
             }
         });
+
+        userWindow.textProperty().addListener((obs, oldText, newText) -> {
+            updateHighlighting();
+        });
     }
+
     private double compareCode(String userCode, String referenceCode) {
         // Простейший алгоритм Левенштейна
         int[][] dp = new int[userCode.length() + 1][referenceCode.length() + 1];
@@ -164,19 +178,81 @@ public class MainFormController implements Initializable {
         return String.format("%02d:%02d:%02d", hours, minutes, seconds);
     }
 
+    private String generatedCode = ""; // Добавьте поле для хранения сгенерированного кода
 
     private void generateCode() {
         String selectedLanguage = comboBox.getValue();
         if (selectedLanguage != null) {
             // Получение рандомного кода для выбранного языка и отображение его в codeWindow
             String randomCode = databaseManager.getRandomCode(selectedLanguage);
-            codeWindow.setText(randomCode);
+            codeWindow.replaceText(randomCode);
+            generatedCode = randomCode; // Сохраните сгенерированный код
+
+            // Вызов метода для обновления подсветки в codeWindow
+            updateHighlighting();
         }
     }
+
 
     public void setUserId(Integer userId) {
         this.userId = userId;
         initData();
     }
-}
 
+    private void updateHighlighting() {
+        userWindow.setStyleSpans(0, computeHighlighting(userWindow.getText()));
+        codeWindow.setStyleSpans(0, computeHighlighting(generatedCode)); // Подсветите сгенерированный код
+    }
+
+    private static final String[] KEYWORDS = new String[] {
+            "abstract", "assert", "boolean", "break", "byte",
+            "case", "catch", "char", "class", "const",
+            "continue", "default", "do", "double", "else",
+            "enum", "extends", "final", "finally", "float",
+            "for", "if", "goto", "implements", "import",
+            "instanceof", "int", "interface", "long", "native",
+            "new", "package", "private", "protected", "public",
+            "return", "short", "static", "strictfp", "super",
+            "switch", "synchronized", "this", "throw", "throws",
+            "transient", "try", "void", "volatile", "while"
+    };
+
+    private static final String KEYWORD_PATTERN = "\\b(" + String.join("|", KEYWORDS) + ")\\b";
+    private static final String PAREN_PATTERN = "\\(|\\)";
+    private static final String BRACE_PATTERN = "\\{|\\}";
+    private static final String BRACKET_PATTERN = "\\[|\\]";
+    private static final String SEMICOLON_PATTERN = "\\;";
+    private static final String STRING_PATTERN = "\"([^\"\\\\]|\\\\.)*\"";
+    private static final String COMMENT_PATTERN = "//[^\n]*" + "|" + "/\\*(.|\\R)*?\\*/";
+
+    private static final Pattern PATTERN = Pattern.compile(
+            "(?<KEYWORD>" + KEYWORD_PATTERN + ")"
+                    + "|(?<PAREN>" + PAREN_PATTERN + ")"
+                    + "|(?<BRACE>" + BRACE_PATTERN + ")"
+                    + "|(?<BRACKET>" + BRACKET_PATTERN + ")"
+                    + "|(?<SEMICOLON>" + SEMICOLON_PATTERN + ")"
+                    + "|(?<STRING>" + STRING_PATTERN + ")"
+                    + "|(?<COMMENT>" + COMMENT_PATTERN + ")"
+    );
+    private static StyleSpans<Collection<String>> computeHighlighting(String text) {
+        Matcher matcher = PATTERN.matcher(text);
+        int lastKwEnd = 0;
+        StyleSpansBuilder<Collection<String>> spansBuilder = new StyleSpansBuilder<>();
+        while(matcher.find()) {
+            String styleClass =
+                    matcher.group("KEYWORD") != null ? "keyword" :
+                            matcher.group("PAREN") != null ? "paren" :
+                                    matcher.group("BRACE") != null ? "brace" :
+                                            matcher.group("BRACKET") != null ? "bracket" :
+                                                    matcher.group("SEMICOLON") != null ? "semicolon" :
+                                                            matcher.group("STRING") != null ? "string" :
+                                                                    matcher.group("COMMENT") != null ? "comment" :
+                                                                            null; /* never happens */ assert styleClass != null;
+            spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
+            spansBuilder.add(Collections.singleton(styleClass), matcher.end() - matcher.start());
+            lastKwEnd = matcher.end();
+        }
+        spansBuilder.add(Collections.emptyList(), text.length() - lastKwEnd);
+        return spansBuilder.create();
+    }
+}
